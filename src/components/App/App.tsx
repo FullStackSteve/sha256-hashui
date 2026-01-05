17b09ed1ab78d5d57fce29b478b1e2f5d5d45eb0
FullStackSteve <stevejamesdaniels@outlook.com>
2026-01-05 15:08:31 +0000

    POC progress bar

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,37 +1,77 @@
-import { useState } from 'react'
-import reactLogo from '../../assets/react.svg'
-import viteLogo from '/vite.svg'
+import { useEffect, useEffectEvent, useState } from 'react'
+import {
+  Container,
+  Box,
+  Typography,
+  Button,
+  LinearProgress,
+} from '@mui/material'
 import './App.css'
 
 export function App() {
-  const [count, setCount] = useState(0)
+  const [progress, setProgress] = useState(0)
+  const [isEncoding, setIsEncoding] = useState(false)
+  const [hasEncoded, setHasEncoded] = useState(false)
+  const [error, setError] = useState<string | null>(null)
+  const [file, setFile] = useState<File | null>(null)
+  const [fileChunks, setFileChunks] = useState<File[]>([])
+
+  const handleEncoded = useEffectEvent(() => {
+    setIsEncoding(false)
+    setHasEncoded(true)
+  })
+
+  useEffect(() => {
+    if (isEncoding && !hasEncoded) {
+      let timer: NodeJS.Timeout
+      if (progress < 100) {
+        timer = setTimeout(() => {
+          setProgress((prev) => Math.min(prev + 10, 100))
+        }, 500)
+      } else {
+        handleEncoded()
+      }
+      return () => {
+        if (timer) clearTimeout(timer)
+      }
+    }
+  }, [hasEncoded, isEncoding, progress])
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1 data-testid="heading">Vite + React</h1>
-      <div className="card">
-        <button
-          data-testid="button-count"
-          onClick={() => setCount((count) => count + 1)}
-        >
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <Container maxWidth="sm">
+      <Box sx={{ my: 4 }}>
+        <Typography variant="h2" component="h1" sx={{ mb: 2 }}>
+          Sha256 Encoder
+        </Typography>
+        {!hasEncoded && !isEncoding && (
+          <Button
+            data-testid="encode-button"
+            variant="contained"
+            onClick={() => setIsEncoding(true)}
+            sx={{ mb: 2 }}
+          >
+            Encode file
+          </Button>
+        )}
+        {isEncoding && (
+          <LinearProgress
+            data-testid="encode-progress"
+            variant="determinate"
+            value={progress}
+          />
+        )}
+        {hasEncoded && (
+          <Typography variant="h6" component="p" data-testid="encode-success">
+            File encoded successfully!
+          </Typography>
+        )}
+        {error && (
+          <Typography color="error" data-testid="encode-error">
+            {error}
+          </Typography>
+        )}
+      </Box>
+    </Container>
   )
 }
 
