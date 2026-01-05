a691ec258b70e95b44d15c0174f0241f877ca377
FullStackSteve <stevejamesdaniels@outlook.com>
2026-01-05 13:55:13 +0200

    lint and format

--- eslint.config.js
+++ eslint.config.js
@@ -3,7 +3,9 @@ import globals from 'globals'
 import reactHooks from 'eslint-plugin-react-hooks'
 import reactRefresh from 'eslint-plugin-react-refresh'
 import tseslint from 'typescript-eslint'
+import eslintConfigPrettier from 'eslint-config-prettier'
 import { defineConfig, globalIgnores } from 'eslint/config'
+import prettier from 'eslint-plugin-prettier/recommended'
 
 export default defineConfig([
   globalIgnores(['dist']),
@@ -14,6 +16,8 @@ export default defineConfig([
       tseslint.configs.recommended,
       reactHooks.configs.flat.recommended,
       reactRefresh.configs.vite,
+      prettier,
+      eslintConfigPrettier,
     ],
     languageOptions: {
       ecmaVersion: 2020,
